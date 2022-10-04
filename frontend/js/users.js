1e008c122cb0212a82a06262590e8534f8d1eb22
HawraaYounes <hawraayounes0@gmail.com>
2022-10-04 04:30:02 +0300

    Getting unblocked users dynamically

--- frontend/js/users.js
+++ frontend/js/users.js
@@ -2,6 +2,7 @@
     const baseUrl="http://127.0.0.1:8000/api";
     //initialize variables
     const token=localStorage.getItem("token");
+    const usersContainer=document.querySelector(".users-container");
     //fetch login api
     const getUsers= ()=>{
         const getUsersAPI=`${baseUrl}/getUsers`;
@@ -9,7 +10,35 @@
         data.append("token", token);
               axios.post(getUsersAPI,data).then(
                 response =>  {
-                   console.log(response.data);
+
+                   response.data.data.forEach(element => {
+                    const userDiv=document.createElement("div");
+                    userDiv.classList.add("container-flex");
+                    userDiv.classList.add("user");
+                    usersContainer.appendChild(userDiv);
+                    const img=document.createElement("img");
+                    img.src = "./images/default.jpg";
+                    img.classList.add("user-img");
+                    userDiv.appendChild(img);
+                    const userInfo=document.createElement("div");
+                    userInfo.classList.add("user-info");
+                    userDiv.appendChild(userInfo);
+                    const nameText=document.createElement("p");
+                    nameText.classList.add("user-name");
+                    nameText.innerText=element.name;
+                    const locationText=document.createElement("p");
+                    locationText.innerText=element.location;
+                    userInfo.appendChild(nameText);
+                    userInfo.appendChild(locationText);
+                    const msg=document.createElement("p");
+                    userDiv.appendChild(msg);
+                    const icon=document.createElement("i");
+                    icon.classList.add("msg-icon");
+                    icon.classList.add("fa-brands");
+                    icon.classList.add("fa-facebook-messenger");
+                    msg.appendChild(icon);
+                    
+                   });
                 }
             );
     }
